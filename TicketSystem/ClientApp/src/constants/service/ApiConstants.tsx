e7fb0ac19444767cba96ff3dc70e261c24837aed
Seyyid Yiğit <seyyid364@gmail.com>
2022-07-31 14:03:58 +0300

    Endpointler düzenlendi. Default url düzenlendi.

--- TicketSystem/ClientApp/src/constants/service/ApiConstants.tsx
+++ TicketSystem/ClientApp/src/constants/service/ApiConstants.tsx
@@ -1,6 +1,6 @@
 export let servicesBaseUrl = "/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    servicesBaseUrl = "http://localhost:5001/";
+    servicesBaseUrl = "http://localhost:5000/";
 }
 const endpointList = require('./endpoints.json');
 let environment = process.env.REACT_APP_ENVIRONMENT || 'development';
